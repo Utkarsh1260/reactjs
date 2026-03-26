3ecab7a561dc35561fe7539a8cd176ad07d79304
Utkarsh1260 <utkarsh.cyber01@gmail.com>
2026-03-26 19:02:27 +0530

    preview image added

---
+++ css_use_tutorial/src/components/button/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './Button.module.css'
+
+const Button = () => {
+  return (
+    <div  >
+ 
+      <h1>Hello Utkarsh</h1> 
+      <button className={styles.btn}>View Profile</button>
+ 
+    </div>
+  )
+}
+
+export default Button
---
+++ css_use_tutorial/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+
+const Header = () => {
+  return (
+    <div className={styles.btn}>
+      <h2>Hello I am navbar</h2>
+      <button>Open Now</button>
+    </div>
+  )
+}
+
+export default Header
